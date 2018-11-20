3e2dfb974f62bf77d0f5b4a37971ff9acfd2e445
tyler303047 <tyler303047@yahoo.com>
2018-11-20 00:15:40 -0800

    added structure of main state machine

--- Servo_Controller.py
+++ Servo_Controller.py
@@ -1,7 +1,57 @@
 #imports
+from Servo_Control import *
+import tensorflow
+import cv2
+import time
+
+SLEEP_LENGTH_1 = 1
+state_now = 'Encounter'
 
 #functions
+def Find_Pokemon:
+    
+
+#State Machine Functions
+def Encounter:
+    while(True):
+        L_Button()
+        if(Find_Pokemon()):
+            return
+        R_Button()
+        if(Find_Pokemon()):
+            return
+def Run:
+    #TODO: Add button presses I need
+
+def Battle:
+    while(True):
+        A_Button()
+        time.sleep(SLEEP_LENGTH_1)
+        A_Button()
+        time.sleep(SLEEP_LENGTH_1)
+        if not Found_Pokemon():
+            return
+
+def Capture:
+    D_Button()
+    time.sleep(SLEEP_LENGTH_1)
+    A_Button()
+    time.sleep(SLEEP_LENGTH_1)
 
 #main
 def main:
-    
+    #start stage
+    states = {
+        'Encounter': Encounter,
+        'Run': Run,
+        'Battle': Battle,
+        'Capture': Capture
+        }
+
+    #
+    while(True):
+        states[state_now]()
+        
+if __name__ == "__main__":
+    main()
+
